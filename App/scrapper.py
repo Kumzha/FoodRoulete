e4a81e5e88a72ff608d4e4ca6357b71bd764640e
Kumzha <kumza.dominykas@gmail.com>
2024-07-23 13:44:40 +0300

    Update scrapper.py

--- App/scrapper.py
+++ App/scrapper.py
@@ -2,12 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 
-# PATH = "C:/Users/kumza/Desktop/chrome-win64/chrome.exe"
-PATH = "C:\\Users\\kumza\\Desktop\\Programing\\msedgedriver.exe"
 
-edge_options = Options()
+PATH = "C:/Users/kumza/Desktop/Programing/tools/chromedriver-win64/chromedriver.exe"
+# PATH = "C:\\Users\\kumza\\Desktop\\Programing\\msedgedriver.exe"
 
+
+
+# Create Chrome WebDriver options
+chrome_options = Options()
+# Uncomment the next line to run Chrome in headless mode
+# chrome_options.add_argument("--headless")
+
+# Initialize the Chrome WebDriver with the service and options
 service = Service(PATH)
-driver = webdriver.Edge(service=service, options=edge_options)
+print("asd")
+driver = webdriver.Chrome(service=service, options=chrome_options)
+print("yo")
+try:
+    # Open a webpage
+    driver.get("https://www.youtube.com")
 
-driver.get("http://www.youtube.com")
+    # Print the title of the page to confirm successful navigation
+    print(driver.title)
+finally:
+    # Close the WebDriver
+    driver.quit()
