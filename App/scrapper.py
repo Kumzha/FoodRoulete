8e38665274d334b7de9992af50e8e31858f4d886
Kumzha <kumza.dominykas@gmail.com>
2024-07-28 13:46:36 +0300

    Working on scrapper

--- App/scrapper.py
+++ App/scrapper.py
@@ -1,29 +1,67 @@
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import schemas
 
 
-PATH = "C:/Users/kumza/Desktop/Programing/tools/chromedriver-win64/chromedriver.exe"
-# PATH = "C:\\Users\\kumza\\Desktop\\Programing\\msedgedriver.exe"
+PATH = "C:/Users/User/Desktop/Programs/chromedriver-win64/chromedriver.exe"
 
 
 
-# Create Chrome WebDriver options
-chrome_options = Options()
-# Uncomment the next line to run Chrome in headless mode
-# chrome_options.add_argument("--headless")
+chrome_options = webdriver.ChromeOptions()
+service = Service(ChromeDriverManager().install())
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument('--no-sandbox')
 
-# Initialize the Chrome WebDriver with the service and options
-service = Service(PATH)
-print("asd")
-driver = webdriver.Chrome(service=service, options=chrome_options)
-print("yo")
-try:
-    # Open a webpage
-    driver.get("https://www.youtube.com")
+def scrapeBolt(adress: str) -> list[schemas.Restaurant]:
+    try:
+        driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    # Print the title of the page to confirm successful navigation
-    print(driver.title)
-finally:
-    # Close the WebDriver
-    driver.quit()
+        driver.get("https://food.bolt.eu/en-US/landing")
+
+        address_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Enter your address"]'))
+        )
+
+        address_input.send_keys(adress)
+
+        # Wait for the suggestions to appear and click the first one
+        first_suggestion = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, 'r-w7s2jr'))
+        )
+        first_suggestion.click()
+
+        time.sleep(5)
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # driver.execute_script("window.scrollBy(0, 1000);")
+        
+        time.sleep(5)
+        html_content = driver.page_source
+
+        file_path = 'pagehtml.txt'
+
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(html_content)
+
+        # search_results = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div/div/div/div/div/div/div/div[3]/div/div/div/div[2]/div/div[16]/div/div/div/a/div/div/div[2]/div/div[1]/div/span" )
+        # print(search_results.text)
+        # for result in search_results:
+        #     print(result.text)
+
+
+        time.sleep(5) 
+
+    finally:
+        driver.quit()
+
+    return[{"name": "Bolt"}]
+
+scrapeBolt("Pavasario g. 30")
--- App/utils.py
+++ App/utils.py
@@ -1,10 +1,15 @@
 # from scrapper import WoltScrapper, BoltScrapper
 import schemas, models, database, crud
+from scrapper import scrapeBolt
 from sqlalchemy.orm import Session
 
 def generateRestaurants(payload: schemas.userSelection, db: Session) -> list[schemas.Restaurant]:
     print(payload.userAdress)
     
-    mylist = []
-    mylist.append({"name": "name3"})
-    return mylist
+    #TODO implement wolt/bolt check
+
+    restaurants = scrapeBolt(payload.userAdress)
+
+    print(restaurants)
+
+    return restaurants
