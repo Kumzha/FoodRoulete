976bc6f97c9548d69e7e5ffe2f01b993efdc637d
Kumzha <kumza.dominykas@gmail.com>
2024-08-01 22:59:21 +0300

    added grid, and nested searchbar+providerselection in a div(mini adjustment)

--- nextjs/src/app/page.js
+++ nextjs/src/app/page.js
@@ -5,15 +5,17 @@ export default function Page() {
 
 
 
-  return <div className="pl-12 max-w-6xl mx-auto">
-            <h1 className="text-6xl w-3/4 mt-28 font-bold" >Choose food faster!</h1>
-            <h2 className="text-3xl mt-5">It's easier to choose food while playing games</h2>
-            <SearchBar/>
-            <div className="flex mt-10 ml-1">
-              <ProviderSelection provider="bolt"/>
-              <ProviderSelection provider="wolt"/>
+  return <div className="pl-6 max-w-6xl mx-auto grid grid-cols-1 gap-5 mt-32">
+            <h1 className="text-6xl w-3/4 font-bold" >Choose food faster!</h1>
+            <h2 className="text-3xl">It's easier to choose food while playing games</h2>
+            <div className="mt-24 ">
+              <SearchBar/>
+              <div className="flex mt-5">
+                <ProviderSelection provider="bolt"/>
+                <ProviderSelection provider="wolt"/>
+              </div>
             </div>
-            <div className="flex justify-between mx-auto mt-10 items-center">
+            <div className="flex justify-between mx-auto items-center">
               <button className="bg-gray-500 text-white px-5 py-2 mx-auto rounded-md">Start</button>
             </div>
          </div>;
--- nextjs/src/components/SearchBar.jsx
+++ nextjs/src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ export default function SearchBar() {
       };
 
   return (
-    <form className="mt-20 ">
+    <form className="">
         <div className='ml-10 w-1/2 p-1 pl-5 border-zinc-800 border-2 rounded-lg'>
             <IoSearchSharp className='inline'/>
             <input className="w-10/12 p-1 ml-5" type="text" placeholder='Enter your address: ' style={inputStyle}/>
